f3c2b70bdd1609df6967d6054d416509762dcdb7
Android Developeruz <rmamajonov002@gmail.com>
2023-09-23 20:44:42 +0500

    Math_Functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+import kotlin.math.E
+import kotlin.math.PI
+
+fun main(args: Array<String>) {
+
+    val firstNum = 10 // Buyerda firstNum va 10 o'rniga hohlagan ma'lumotingizni kiritishingiz mumkin
+    val doubleNum = 20 // Buyerda doubleNum va 20 o'rniga hohlagan ma'lumotingizni kiritishingiz mumkin
+
+    val equals1 = firstNum + doubleNum // Bu qatorda firstNum qiymatiga doubleNum qiymatiga qo'shadi
+    val equals2 = firstNum - doubleNum // Bu qatorda firstNum qiymatidan doubleNum qiymatni ayriydi
+    val equals3 = firstNum / doubleNum // Bu qatorda firstNum qiymatini doubleNum qiymatga bo'ladi
+    val equals4 = firstNum * doubleNum // Bu qatorda firstNum qiymatini doubleNum qiymatga ko'paytiradi
+
+    val pi = PI // BU qator matematikadagi PI amalini chiqaradi
+    val e = E // BU qator matematikadagi E amalini chiqaradi
+
+    // Bu qatorlarda biz hisoblagan matematik amallarni consul oynasiga chiqaradi
+    print(equals1)
+    print(equals2)
+    print(equals3)
+    print(equals4)
+    print(pi)
+    print(e)
+
+}
